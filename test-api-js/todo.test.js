20e35b55ad263491fa38c23f33a13db52b44b1de
Deepakchiluveru <deepak.chiluveru@slu.edu>
2022-11-07 22:30:16 -0600

    testcase for add function

--- test-api-js/todo.test.js
+++ test-api-js/todo.test.js
@@ -1,27 +1,49 @@
-
-
 var todoservice = require('./todo.service.js');
 describe('todo test suite', () => {
 
     test("truth_value", () => {
         expect(true).toBe(true);
     });
-    
+
     let todo_service = new todoservice();
 
     test("if service instance is created", () => {
         expect(todo_service instanceof todoservice).toBe(true);
     });
 
-    
+
     // Initial length of the todo list is 3 // 3 default tasks
     test("get_todos", () => {
         expect(todo_service.get_todos().todo.length).toEqual(3);
     });
+    // test case for add
 
-    
+    const add_test = [{
+        "title": "DeepakAdd1",
+        "description": "Deepu1",
+        "done": false
+    },
+    {
+        "title": "DeepakAdd2",
+        "description": "Deepu2",
+        "done": true
+    },
+    {
+        "title": "DeepakAdd3",
+        "description": "Deepu3",
+        "done": false
+    }
+    ];
 
-    // Write all your test cases here that corresponds to software requirements
 
 
-});
+    // test case for add
+
+
+
+    test("add-todo", () => {
+        add_test.forEach(function (obj) {
+            expect(todo_service.add_todo(obj)).toEqual(obj);
+        })
+    });
+})
