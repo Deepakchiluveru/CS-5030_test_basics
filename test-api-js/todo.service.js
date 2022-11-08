ffa6b51828dcd622b129c03e62c6efbc8305ad9f
Deepakchiluveru <deepak.chiluveru@slu.edu>
2022-11-07 22:23:04 -0600

    todo functions

--- test-api-js/todo.service.js
+++ test-api-js/todo.service.js
@@ -1,39 +1,44 @@
-class todoservice{
+class todoservice {
     todo_data = {
-        "todo":[{
-            "title": "T1",
-            "description": "D1",
+        "todo": [{
+            "title": "learning ",
+            "description": "JS",
             "done": false
-        },{
-            "title": "T1",
-            "description": "D1",
+        }, {
+            "title": "Reading",
+            "description": "Novel ",
             "done": false
-        },{
-            "title": "T1",
-            "description": "D1",
+        }, {
+            "title": "Playing",
+            "description": "Cricket",
             "done": false
         }]
     }
-    constructor(){
-        this.todos=this.todo_data;
+    constructor() {
+        this.todos = this.todo_data;
     }
 
-    get_todos(){
+    get_todos() {
         return this.todos;
     }
 
-    add_todo(todo){
+    add_todo(todo) {
         // Your code here
+        this.todo_data.todo.push(todo);
+        return this.todo_data.todo[this.todo_data.todo.length - 1];
+    }
+
+    update_todo(id, todo) {
+        // Your code here
+        this.todo_data.todo[id] = todo;
+        return this.todo_data.todo[id];  
     }
 
     delete_todo(id){
         // Your code here
-    }
-
-    update_todo(id, todo){
-        // Your code here
-    }
+    this.todos = this.todo_data.todo.pop(id);
+    return this.todo_data;
 }
+}
+module.exports = todoservice;
 
-
-module.exports= todoservice;
